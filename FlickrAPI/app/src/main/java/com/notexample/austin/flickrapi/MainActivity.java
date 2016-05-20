ee0f07746ef64a3d2c20393732b819408b0a97b8
aharshbe <a.catawampus@gmail.com>
2016-05-19 19:03:08 -0700

    Adding more searchs tuff :

--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
@@ -2,12 +2,32 @@ package com.notexample.austin.flickrapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchView = (SearchView) findViewById(R.id.searchView);
+        searchView.setQueryHint("Search Me for Images");
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        return true;
     }
 }
