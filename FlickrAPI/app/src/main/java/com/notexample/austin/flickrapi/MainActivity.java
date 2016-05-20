83e6e86af40691c65f6c14039154bf0888e6a203
aharshbe <a.catawampus@gmail.com>
2016-05-19 19:11:23 -0700

    Adding manifest singletop thing and fixing the search manager

--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
@@ -1,5 +1,7 @@
 package com.notexample.austin.flickrapi;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
@@ -8,18 +10,11 @@ import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
-    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        searchView = (SearchView) findViewById(R.id.searchView);
-        searchView.setQueryHint("Search Me for Images");
-
-
-
 
     }
 
@@ -28,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
 
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
         return true;
     }
+
+
 }
