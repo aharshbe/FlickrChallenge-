272c5945ffb2003f816823dfd0d7fa96b062bc52
aharshbe <a.catawampus@gmail.com>
2016-05-20 12:40:01 -0700

    got the groundwork to go

--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/Main2Activity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/Main2Activity.java
@@ -2,12 +2,20 @@ package com.notexample.austin.flickrapi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        final String image = getIntent().getStringExtra("image");
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText(image);
     }
+
+
 }
--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
@@ -13,6 +13,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -31,6 +33,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.util.LinkedList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -38,7 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
     SearchView searchView;
-    TextView button, idtv, fulltv;
+    TextView button;
+    LinkedList<String> items, item2;
+    ArrayAdapter<String> mAdapter, mAdapter2;
+    ListView listView, listView2;
 
 
 
@@ -53,11 +59,20 @@ public class MainActivity extends AppCompatActivity {
         searchView = (SearchView) findViewById(R.id.search);
 
         button = (TextView) findViewById(R.id.button);
-        idtv = (TextView) findViewById(R.id.id);
-        fulltv = (TextView) findViewById(R.id.all);
+        items = new LinkedList<>();
+        mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(mAdapter);
+
+        item2 = new LinkedList<>();
+        mAdapter2 = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, item2);
+        listView2 = (ListView) findViewById(R.id.listView2);
+        listView2.setAdapter(mAdapter2);
+
 
 
         final AsyncHttpClient client = new AsyncHttpClient();
+        final AsyncHttpClient client2 = new AsyncHttpClient();
         client.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=cf930e4ef3a4a52af4ee0a6fe69b6b61&format=json&text=ugly&nojsoncallback=1", new JsonHttpResponseHandler() {
 
 
@@ -68,16 +83,20 @@ public class MainActivity extends AppCompatActivity {
 
                 progressBar.setVisibility(View.VISIBLE);
 
-                String id, full ;
                 try {
                     JSONObject jsonObject = responseBody.getJSONObject("photos");
                     JSONArray jsonArray = jsonObject.getJSONArray("photo");
                     JSONObject jsonObject1 = jsonArray.getJSONObject(0);
-                    full = jsonObject1.toString();
-                    id = jsonObject1.getString("id");
-                    idtv.setText(id);
-                    fulltv.setText(full);
                     progressBar.setVisibility(View.GONE);
+                    items.add(jsonObject1.getString("id"));
+                    items.add(jsonArray.getJSONObject(1).getString("id"));
+                    items.add(jsonArray.getJSONObject(2).getString("id"));
+                    items.add(jsonArray.getJSONObject(5).getString("id"));
+                    items.add(jsonArray.getJSONObject(6).getString("id"));
+                    items.add(jsonArray.getJSONObject(7).getString("id"));
+                    items.add(jsonArray.getJSONObject(8).getString("id"));
+                    items.add(jsonArray.getJSONObject(9).getString("id"));
+                    items.add(jsonArray.getJSONObject(10).getString("id"));
 
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -89,6 +108,54 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+        client2.get("https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=cf930e4ef3a4a52af4ee0a6fe69b6b61&photo_id=27098439566&format=json&nojsoncallback=1", new JsonHttpResponseHandler() {
+
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject responseBody) {
+                Toast.makeText(getApplicationContext(), "Process Successful",
+                        Toast.LENGTH_SHORT).show();
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                try {
+                    JSONObject jsonObject = responseBody.getJSONObject("photo");
+                    JSONObject jsonObject2 = jsonObject.getJSONObject("urls");
+                    JSONArray jsonArray = jsonObject2.getJSONArray("url");
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(0);
+                    item2.add(jsonObject1.getString("_content"));
+                    item2.add(jsonArray.getJSONObject(1).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(2).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(5).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(6).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(7).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(8).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(9).getString("_content"));
+                    item2.add(jsonArray.getJSONObject(10).getString("_content"));
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+                myIntent.putExtra("position", position);
+                String imageid = items.get(position);
+                myIntent.putExtra("image", imageid);
+                startActivity(myIntent);
             }
         });
     }
