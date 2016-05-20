059324cb384078953e739ce8df53e4f497457477
aharshbe <a.catawampus@gmail.com>
2016-05-19 20:21:55 -0700

    Adding information before heading to bed

--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
@@ -3,17 +3,34 @@ package com.notexample.austin.flickrapi;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.*;
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -21,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
     SearchView searchView;
+    TextView button;
+    ListView listView;
 
 
     @Override
@@ -31,14 +50,47 @@ public class MainActivity extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
 
+        searchView = (SearchView) findViewById(R.id.search);
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
+        button = (TextView) findViewById(R.id.button);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+
+
+
+        final AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=cf930e4ef3a4a52af4ee0a6fe69b6b61&format=json&text=pretty", new AsyncHttpResponseHandler() {
+
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                Toast.makeText(getApplicationContext(), "Process Successful",
+                        Toast.LENGTH_SHORT).show();
 
+
+//                try {
+//                    String jsonStr = new String(responseBody, "UTF-8");
+//                    Log.e("Tag ","jsonStr "+jsonStr);
+//
+//                } catch (UnsupportedEncodingException e) {
+//                    e.printStackTrace();
+//                }
+//                InputStream is = null;
+//                String contentAsString = null;
+//                try {
+//                    contentAsString = readIt(is);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                String parsingData = null;
+//                try {
+//                    parsingData = parseJson(contentAsString);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
             }
+
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
@@ -75,6 +127,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    public void checkingCOnnection(View view) {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            Toast.makeText(getApplicationContext(), "Connection ready",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Connection not ready",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+    public String readIt(InputStream stream) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        String read;
+
+        while ((read = br.readLine()) != null) {
+            sb.append(read);
+        }
+        return sb.toString();
+    }
+
+    private String parseJson(String contentAsString) throws JSONException {
+        String postList = "";
+        JSONArray array = new JSONArray(contentAsString);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject post = array.getJSONObject(i);
+            String postTitle = post.getString("pages");
+
+            postList += postTitle;
+        }
+        return postList;
+    }
+
+
+
 
 }
 
