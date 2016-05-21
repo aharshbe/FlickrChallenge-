68760b10025cfcfd1b6effc1f8bc2a24b09d3485
aharshbe <a.catawampus@gmail.com>
2016-05-21 14:25:59 -0700

    fixing the edit text annoying problem with the listView

--- FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
+++ FlickrAPI/app/src/main/java/com/notexample/austin/flickrapi/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
